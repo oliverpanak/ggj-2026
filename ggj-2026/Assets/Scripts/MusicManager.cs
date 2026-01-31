8798f6fd9e1bde20ec41fd038af5a02fa181a48d
Byter <byetr64@gmx.de>
2026-01-31 18:41:31 +0100

    Added: FMOD

--- ggj-2026/Assets/Scripts/MusicManager.cs
+++ ggj-2026/Assets/Scripts/MusicManager.cs
@@ -1,33 +1,41 @@
 using System.Collections;
 using UnityEngine;
+using FMODUnity;
 
 public class MusicManager : MonoBehaviour
 {
-    [SerializeField] private AudioSource source;
-
-    [SerializeField] private AudioClip[] clips;
     [SerializeField] private float[] durations;
+    [SerializeField] private StudioEventEmitter eventEmitter;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        StartCoroutine(TestAudio(0));
+        var result = eventEmitter.EventInstance.setParameterByName("Sections", 1f);
+        StartCoroutine(StartDelayed(1));
     }
 
-    private IEnumerator TestAudio(int index)
+    private IEnumerator StartDelayed(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        int index = Random.Range(0, durations.Length);
+        StartCoroutine(SetAudio(index));
+    }
+
+    private IEnumerator SetAudio(int index)
     {
         float time = 0;
 
-        Debug.Log($"Playing {clips[index].name} with duration {durations[index]}");
-        source.PlayOneShot(clips[index]);
+        var result = eventEmitter.EventInstance.setParameterByName("Sections", (float)(index + 1));
+        Debug.Log($"setting track to index {index}");
 
-        while(time < durations[index])
+        while (time < durations[index])
         {
             time += Time.deltaTime;
             yield return null;
         }
 
         index = Random.Range(0, durations.Length);
-        StartCoroutine(TestAudio(index));
+        StartCoroutine(SetAudio(index));
     }
 }

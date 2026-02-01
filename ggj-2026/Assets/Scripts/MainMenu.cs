94b5709230cdd37aa9f4c534fd1c569482b3de1e
Oliver Panak <oliverpanak@gmail.com>
2026-02-01 14:02:32 +0100

    manual testing works

--- ggj-2026/Assets/Scripts/MainMenu.cs
+++ ggj-2026/Assets/Scripts/MainMenu.cs
@@ -4,10 +4,16 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private string gameSceneName = "MainScene01";
+    [SerializeField] private string manualTestingSceneName = "MainScene01_ManualTesting";
 
     public void StartGame()
     {
         SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void StartManualTesting()
+    {
+        SceneManager.LoadScene(manualTestingSceneName);
     }
 
     public void QuitGame()
--- ggj-2026/Assets/Scripts/PlayerSpawnManager.cs
+++ ggj-2026/Assets/Scripts/PlayerSpawnManager.cs
@@ -17,35 +17,44 @@ public class PlayerSpawnManager : MonoBehaviour
 
     public void OnPlayerJoined(PlayerInput playerInput)
     {
-    GameObject newPlayer = playerInput.gameObject;
+        GameObject newPlayer = playerInput.gameObject;
 
-    // Assign color
-    int colorIndex = players.Count % playerColors.Length;
-    AssignPlayerColor(newPlayer, playerColors[colorIndex]);
-
-    // If this is not the first player, connect NEW player to PREVIOUS player
-    if (players.Count > 0)
-    {
-        GameObject previousPlayer = players[players.Count - 1];
+        // Assign color
+        int colorIndex = players.Count % playerColors.Length;
+        AssignPlayerColor(newPlayer, playerColors[colorIndex]);
 
         SpringJoint spring = newPlayer.GetComponent<SpringJoint>();
-        if (spring != null)
+
+        // If this is the first player, disable the spring joint (single player support)
+        if (players.Count == 0)
         {
-            spring.connectedBody = previousPlayer.GetComponent<Rigidbody>();
+            if (spring != null)
+            {
+                Destroy(spring);
+            }
+        }
+        else
+        {
+            // Connect NEW player to PREVIOUS player
+            GameObject previousPlayer = players[players.Count - 1];
+
+            if (spring != null)
+            {
+                spring.connectedBody = previousPlayer.GetComponent<Rigidbody>();
+            }
+
+            // Optional: still keep your line renderer chain
+            PhysicsPlayerController prevController = previousPlayer.GetComponent<PhysicsPlayerController>();
+            if (prevController != null)
+            {
+                prevController.NextPlayer = newPlayer.transform;
+            }
         }
 
-        // Optional: still keep your line renderer chain
-        PhysicsPlayerController prevController = previousPlayer.GetComponent<PhysicsPlayerController>();
-        if (prevController != null)
-        {
-            prevController.NextPlayer = newPlayer.transform;
-        }
+        players.Add(newPlayer);
+
+        Debug.Log($"Player {players.Count} joined and connected");
     }
-
-    players.Add(newPlayer);
-
-    Debug.Log($"Player {players.Count} joined and connected");
-}
 
 
     private void AssignPlayerColor(GameObject player, Color color)
